73274cc21afa62cf27947433bdb2035ce5986493
Samuel Loury <samuel.loury@cosmo-platform.org>
2015-03-04 09:04:09 +0100

    Add a dumb way to save the entered values in the shared preferences when the start button is pressed

--- src/ru/dront78/pulsedroid/PulseDroid.java
+++ src/ru/dront78/pulsedroid/PulseDroid.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.CheckBox;
+import android.content.SharedPreferences;
+import android.content.Context;
 
 public class PulseDroid extends Activity {
 	/** Called when the activity is first created. */
@@ -17,45 +20,55 @@ public class PulseDroid extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
+		final EditText server = (EditText) findViewById(R.id.EditTextServer);
+		final EditText port = (EditText) findViewById(R.id.EditTextPort);
+
+		final SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+		final SharedPreferences.Editor editor = sharedPref.edit();
+
+		server.setText(sharedPref.getString("server", ""));
+		port.setText(sharedPref.getString("port", ""));
+
 		// here is onButtonClick handler
 		final Button playButton = (Button) findViewById(R.id.ButtonPlay);
 		playButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				if (false == playState) {
-					playState = true;
-					playButton.setText("Stop");
-					if (null != playThread) {
-						playThread.Terminate();
-						playThread = null;
-					}
-					final EditText server = (EditText) findViewById(R.id.EditTextServer);
-					final EditText port = (EditText) findViewById(R.id.EditTextPort);
-					playThread = new PulseSoundThread(server.getText()
-							.toString(), port.getText().toString());
-					new Thread(playThread).start();
+				public void onClick(View v) {
+					if (false == playState) {
+						playState = true;
+						playButton.setText("Stop");
+						if (null != playThread) {
+							playThread.Terminate();
+							playThread = null;
+						}
+						editor.putString("server", server.getText().toString());
+						editor.putString("port", port.getText().toString());
+						editor.commit();
+						playThread = new PulseSoundThread(server.getText()
+														  .toString(), port.getText().toString());
+						new Thread(playThread).start();
 
-				} else {
-					playState = false;
-					playButton.setText("Play!");
-					if (null != playThread) {
-						playThread.Terminate();
-						playThread = null;
-					}
-				}
-			}
-		});
-
-		findViewById(R.id.ButtonExit).setOnClickListener(
-				new View.OnClickListener() {
-					public void onClick(View v) {
+					} else {
 						playState = false;
 						playButton.setText("Play!");
 						if (null != playThread) {
 							playThread.Terminate();
 							playThread = null;
 						}
-						moveTaskToBack(true);
 					}
-				});
+				}
+			});
+
+		findViewById(R.id.ButtonExit).setOnClickListener(
+														 new View.OnClickListener() {
+															 public void onClick(View v) {
+																 playState = false;
+																 playButton.setText("Play!");
+																 if (null != playThread) {
+																	 playThread.Terminate();
+																	 playThread = null;
+																 }
+																 moveTaskToBack(true);
+															 }
+														 });
 	}
 }
