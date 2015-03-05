1d2327c0488a76443848d767594fba39d5599ba8
Samuel Loury <samuel.loury@cosmo-platform.org>
2015-03-05 09:41:51 +0100

    Attempt to use a local service to play the music to avoid Android killing the application

--- src/ru/dront78/pulsedroid/PulseDroid.java
+++ src/ru/dront78/pulsedroid/PulseDroid.java
@@ -8,34 +8,91 @@ import android.widget.EditText;
 import android.widget.CheckBox;
 import android.content.SharedPreferences;
 import android.content.Context;
+import android.content.ServiceConnection;
+import android.content.ComponentName;
+import android.os.IBinder;
+import android.widget.Toast;
+import android.app.Notification;
+import android.content.Intent;
+
+import ru.dront78.pulsedroid.LocalService;
+
 
 public class PulseDroid extends Activity {
 	/** Called when the activity is first created. */
 
-	boolean playState = false;
-	PulseSoundThread playThread = null;
+	boolean mIsBound = false;
+	Button playButton = null;
+	private LocalService mBoundService;
+	CheckBox autoStartCheckBox = null;
 
-	public void play() {
+	private ServiceConnection mConnection = new ServiceConnection() {
+			public void onServiceConnected(ComponentName className, IBinder service) {
+				// This is called when the connection with the service has been
+				// established, giving us the service object we can use to
+				// interact with the service.  Because we have bound to a explicit
+				// service that we know is running in our own process, we can
+				// cast its IBinder to a concrete class and directly access it.
+				mBoundService = ((LocalService.LocalBinder)service).getService();
+
+				// Tell the user about this for our demo.
+				Toast.makeText(PulseDroid.this, R.string.local_service_connected,
+							   Toast.LENGTH_SHORT).show();
+				if (autoStartCheckBox.isChecked()) {
+					play();
+				}
+			}
+
+			public void onServiceDisconnected(ComponentName className) {
+				// This is called when the connection with the service has been
+				// unexpectedly disconnected -- that is, its process crashed.
+				// Because it is running in our same process, we should never
+				// see this happen.
+				mBoundService = null;
+				Toast.makeText(PulseDroid.this, R.string.local_service_disconnected,
+							   Toast.LENGTH_SHORT).show();
+			}
+		};
+
+	void doBindService() {
+		// Establish a connection with the service.  We use an explicit
+		// class name because we want a specific service implementation that
+		// we know will be running in our own process (and thus won't be
+		// supporting component replacement by other applications).
+		bindService(new Intent(PulseDroid.this,
+							   LocalService.class), mConnection, Context.BIND_AUTO_CREATE);
+		mIsBound = true;
+	}
+
+	void doUnbindService() {
+		if (mIsBound) {
+			// Detach our existing connection.
+			unbindService(mConnection);
+			mIsBound = false;
+		}
+	}
+
+	public void play () {
 		final Button playButton = (Button) findViewById(R.id.ButtonPlay);
 		final SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
 		final SharedPreferences.Editor editor = sharedPref.edit();
 		final EditText server = (EditText) findViewById(R.id.EditTextServer);
 		final EditText port = (EditText) findViewById(R.id.EditTextPort);
-		final CheckBox autoStartCheckBox = (CheckBox) findViewById(R.id.auto_start);
 
-		playState = true;
-		playButton.setText("Stop");
-		if (null != playThread) {
-			playThread.Terminate();
-			playThread = null;
-		}
 		editor.putString("server", server.getText().toString());
 		editor.putString("port", port.getText().toString());
 		editor.putBoolean("auto_start", autoStartCheckBox.isChecked());
 		editor.commit();
-		playThread = new PulseSoundThread(server.getText()
-										  .toString(), port.getText().toString());
-		new Thread(playThread).start();
+		mBoundService.port = port.getText().toString();
+		mBoundService.server = server.getText().toString();
+
+		playButton.setText("Stop");
+		mBoundService.play();
+	}
+
+	public void stop () {
+		playButton.setText("Play!");
+		mBoundService.stop();
 	}
 
 	@Override
@@ -45,7 +102,7 @@ public class PulseDroid extends Activity {
 
 		final EditText server = (EditText) findViewById(R.id.EditTextServer);
 		final EditText port = (EditText) findViewById(R.id.EditTextPort);
-		final CheckBox autoStartCheckBox = (CheckBox) findViewById(R.id.auto_start);
+		autoStartCheckBox = (CheckBox) findViewById(R.id.auto_start);
 
 		final SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
 		final SharedPreferences.Editor editor = sharedPref.edit();
@@ -55,18 +112,13 @@ public class PulseDroid extends Activity {
 		autoStartCheckBox.setChecked(sharedPref.getBoolean("auto_start", false));
 
 		// here is onButtonClick handler
-		final Button playButton = (Button) findViewById(R.id.ButtonPlay);
+		playButton = (Button) findViewById(R.id.ButtonPlay);
 		playButton.setOnClickListener(new View.OnClickListener() {
 				public void onClick(View v) {
-					if (false == playState) {
+					if (false == mBoundService.playState) {
 						play();
 					} else {
-						playState = false;
-						playButton.setText("Play!");
-						if (null != playThread) {
-							playThread.Terminate();
-							playThread = null;
-						}
+						stop();
 					}
 				}
 			});
@@ -74,18 +126,18 @@ public class PulseDroid extends Activity {
 		findViewById(R.id.ButtonExit).setOnClickListener(
 														 new View.OnClickListener() {
 															 public void onClick(View v) {
-																 playState = false;
-																 playButton.setText("Play!");
-																 if (null != playThread) {
-																	 playThread.Terminate();
-																	 playThread = null;
-																 }
+																 stop();
 																 moveTaskToBack(true);
 															 }
 														 });
-		if (autoStartCheckBox.isChecked()) {
-			play();
-		}
 
+		doBindService();
 	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		doUnbindService();
+	}
+
 }
