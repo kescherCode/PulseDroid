9fee1893003fa073dc8d6a304984e282756266d4
Daniel K <code.danielk@gmail.com>
2018-06-08 22:11:05 +0200

    Refactor large worker method

--- app/src/main/java/ru/dront78/pulsedroid/PulsePlaybackWorker.java
+++ app/src/main/java/ru/dront78/pulsedroid/PulsePlaybackWorker.java
@@ -28,10 +28,20 @@ public class PulsePlaybackWorker implements Runnable {
     private final Handler handler;
     private final Listener listener;
 
-    private Throwable error;
     private volatile boolean stopped = false;
-    private Socket sock;
     private volatile int bufferSizeMillis = 2000;
+
+    private Throwable error;
+    private Socket sock;
+    private InputStream audioData;
+    private AudioTrack audioTrack;
+    private byte[] audioBuffer;
+
+    private int numSkip;
+    private int bufferPos;
+    private int chunkSize;
+    private int minBufferSize;
+    private int byteRate;
 
     PulsePlaybackWorker(String host, int port, WakeLock wakeLock, Handler handler, Listener listener) {
         this.host = host;
@@ -59,116 +69,27 @@ public class PulsePlaybackWorker implements Runnable {
         }
     }
 
-    private void stopWithError(Throwable e) {
-        Log.e(PulsePlaybackWorker.class.getSimpleName(), "stopWithError", e);
-        error = e;
-        stopped = true;
-        handler.post(() -> listener.onPlaybackError(this, e));
-    }
-
     public Throwable getError() {
         return error;
     }
 
     public void run() {
-        InputStream audioData = null;
-        AudioTrack audioTrack = null;
         try {
-            final int sampleRate = 48000;
-            // bytes per second = sample rate * 2 bytes per sample * 2 channels
-            final int byteRate = sampleRate * 2 * 2;
-
-            final int minBufferSize = AudioTrack.getMinBufferSize(sampleRate,
-                    AudioFormat.CHANNEL_CONFIGURATION_STEREO,
-                    AudioFormat.ENCODING_PCM_16BIT);
-
-            // Try to receive 4 times per second.
-            final int chunkSize = byteRate / 4;
-
-            connect();
-            audioData = sock.getInputStream();
-
-            // Always using minimum buffer size for minimum lag.
-            audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-                    sampleRate, AudioFormat.CHANNEL_CONFIGURATION_STEREO,
-                    AudioFormat.ENCODING_PCM_16BIT, minBufferSize,
-                    AudioTrack.MODE_STREAM);
-            if (audioTrack.getState() != AudioTrack.STATE_INITIALIZED) {
-                throw new IllegalStateException(
-                        "Could not initialize AudioTrack."
-                        + " state == " + audioTrack.getState());
-            }
-            audioTrack.play();
+            setup();
 
             boolean started = false;
-
-            int bufPos = 0;
-            int numSkip = 0;
-            byte[] audioBuffer = new byte[chunkSize];
-
             while (!stopped) {
                 wakeLock.acquire(1000);
 
-                // Respect bufferSizeMillis setting. Larger if system needs it.
-                final long bufferSizeMillis = this.bufferSizeMillis;
+                manageBufferSize();
 
-                if (bufferSizeMillis >= 0) {
-                    final int bufferSize = Math.max(minBufferSize, (int) (byteRate * bufferSizeMillis / 1000));
+                readFromSocket();
 
-                    final int available = audioData.available();
-                    if (available > bufferSize) {
-                        // We have more bytes than fit into our buffer. Skip forward so
-                        // we don't get left behind.
-                        // [bufferSize / 2]: Try to keep our buffer half-filled.
-                        final long wantSkip = numSkip + (available - bufferSize / 2);
-                        final long actual = audioData.skip(wantSkip);
-                        // If we happened to skip part of a pair of samples, we need
-                        // to skip the remaining bytes of it when writing to audioTrack.
-                        final int malign = (int) ((bufPos + actual) & 3L);
-                        if (malign != 0) {
-                            numSkip = 4 - malign;
-                        } else {
-                            numSkip = 0;
-                        }
-                        Log.d("Worker", "skipped: wantSkip=" + wantSkip + " actual=" + actual + " numSkip=" + numSkip + " bufPos=" + bufPos);
-                        bufPos = 0;
-                    }
-                }
+                writeToAudioTrack();
 
-                // Never aim to write more than chunkSize to audioTrack, so we don't get
-                // blocked any longer than needed.
-                int wantRead = chunkSize - bufPos;
-
-                int nRead = audioData.read(audioBuffer, bufPos, wantRead);
-                if (nRead < 0) {
-                    throw new EOFException("Connection closed");
-                }
-                bufPos += nRead;
-
-                int writeStart = numSkip;
-                // [& ~3]: Only try to write full sample-pairs.
-                int wantWrite = (bufPos - numSkip) & ~3;
-
-                int sizeWrite = 0;
-                if (wantWrite > 0) {
-                    sizeWrite = audioTrack.write(audioBuffer, writeStart, wantWrite);
-                }
-
-                if (sizeWrite < 0) {
-                    stopWithError(new IOException("audioTrack.write() returned " + sizeWrite));
-                } else {
-                    if (sizeWrite > 0) {
-                        // Move remaining data to the start of the buffer.
-                        int writeEnd = writeStart + sizeWrite;
-                        int len = bufPos - writeEnd;
-                        System.arraycopy(audioBuffer, writeEnd, audioBuffer, 0, len);
-                        bufPos = len;
-                        numSkip = 0;
-                    }
-                    if (!started) {
-                        started = true;
-                        handler.post(() -> listener.onPlaybackStarted(this));
-                    }
+                if (!started) {
+                    started = true;
+                    handler.post(() -> listener.onPlaybackStarted(this));
                 }
             }
 
@@ -177,35 +98,141 @@ public class PulsePlaybackWorker implements Runnable {
             handler.post(() -> listener.onPlaybackStopped(this));
         } catch (Exception e) {
             // Suppress exception caused by stop() closing our socket.
-            if (!stopped || !(e instanceof SocketException)) {
-                stopWithError(e);
-            } else {
+            if (stopped && e instanceof SocketException) {
                 handler.post(() -> listener.onPlaybackStopped(this));
+            } else {
+                Log.e(PulsePlaybackWorker.class.getSimpleName(), "stopWithError", e);
+                error = e;
+                handler.post(() -> listener.onPlaybackError(this, e));
             }
         } finally {
-            if (audioData != null) {
-                try {
-                    audioData.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
+            cleanup();
+        }
+    }
+
+    private void setup() throws IOException {
+        final int sampleRate = 48000;
+        // bytes per second = sample rate * 2 bytes per sample * 2 channels
+        byteRate = sampleRate * 2 * 2;
+
+        minBufferSize = AudioTrack.getMinBufferSize(sampleRate,
+                AudioFormat.CHANNEL_CONFIGURATION_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT);
+
+        // Try to receive 4 times per second.
+        chunkSize = byteRate / 4;
+
+        connect();
+
+        audioData = sock.getInputStream();
+
+        // Always using minimum buffer size for minimum lag.
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_CONFIGURATION_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT, minBufferSize,
+                AudioTrack.MODE_STREAM);
+        if (audioTrack.getState() != AudioTrack.STATE_INITIALIZED) {
+            throw new IllegalStateException(
+                    "Could not initialize AudioTrack."
+                    + " state == " + audioTrack.getState());
+        }
+        audioTrack.play();
+
+        bufferPos = 0;
+        numSkip = 0;
+        audioBuffer = new byte[chunkSize];
+    }
+
+    private void manageBufferSize() throws IOException {
+        final long bufferSizeMillis = this.bufferSizeMillis;
+
+        // size < 0 means infinite buffer: never skip anything
+        if (bufferSizeMillis >= 0) {
+            final int bufferSize = Math.max(minBufferSize, (int) (byteRate * bufferSizeMillis / 1000));
+
+            final int available = audioData.available();
+            if (available > bufferSize) {
+                // We have more bytes than fit into our buffer. Skip forward so
+                // we don't get left behind.
+                // [bufferSize / 2]: Try to keep our buffer half-filled.
+                final long wantSkip = numSkip + (available - bufferSize / 2);
+                final long actual = audioData.skip(wantSkip);
+                // If we happened to skip part of a pair of samples, we need
+                // to skip the remaining bytes of it when writing to audioTrack.
+                final int malign = (int) ((bufferPos + actual) & 3L);
+                if (malign != 0) {
+                    numSkip = 4 - malign;
+                } else {
+                    numSkip = 0;
                 }
+                Log.d("Worker", "skipped: wantSkip=" + wantSkip + " actual=" + actual + " numSkip=" + numSkip + " bufferPos=" + bufferPos);
+                bufferPos = 0;
             }
-            if (sock != null) {
-                try {
-                    sock.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                sock = null;
+        }
+    }
+
+    private void readFromSocket() throws IOException {
+        // Never aim to write more than chunkSize to audioTrack, so we don't get
+        // blocked any longer than needed.
+        int wantRead = chunkSize - bufferPos;
+
+        int nRead = audioData.read(audioBuffer, bufferPos, wantRead);
+        if (nRead < 0) {
+            throw new EOFException("Connection closed");
+        }
+        bufferPos += nRead;
+    }
+
+    private void writeToAudioTrack() throws IOException {
+        int writeStart = numSkip;
+        // [& ~3]: Only try to write full sample-pairs.
+        int wantWrite = (bufferPos - numSkip) & ~3;
+
+        int sizeWrite = 0;
+        if (wantWrite > 0) {
+            sizeWrite = audioTrack.write(audioBuffer, writeStart, wantWrite);
+        }
+
+        if (sizeWrite < 0) {
+            throw new IOException("audioTrack.write() returned " + sizeWrite);
+        } else {
+            if (sizeWrite > 0) {
+                // Move remaining data to the start of the buffer.
+                int writeEnd = writeStart + sizeWrite;
+                int len = bufferPos - writeEnd;
+                System.arraycopy(audioBuffer, writeEnd, audioBuffer, 0, len);
+                bufferPos = len;
+                numSkip = 0;
             }
-            if (audioTrack != null) {
-                // AudioTrack throws if we call stop() in stopped state. This happens if
-                // audioTrack.play() fails.
-                if (audioTrack.getPlayState() != AudioTrack.PLAYSTATE_STOPPED) {
-                    audioTrack.stop();
-                }
-                audioTrack.release();
+        }
+    }
+
+    private void cleanup() {
+        audioBuffer = null;
+        if (audioData != null) {
+            try {
+                audioData.close();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+            audioData = null;
+        }
+        if (sock != null) {
+            try {
+                sock.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            sock = null;
+        }
+        if (audioTrack != null) {
+            // AudioTrack throws if we call stop() in stopped state. This happens if
+            // audioTrack.play() fails.
+            if (audioTrack.getPlayState() != AudioTrack.PLAYSTATE_STOPPED) {
+                audioTrack.stop();
+            }
+            audioTrack.release();
+            audioTrack = null;
         }
     }
 
